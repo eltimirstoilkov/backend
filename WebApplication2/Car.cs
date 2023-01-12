8f0124a2a971bdc8dc0d1c2cd65edad8a9c7397d
eltimirstoilkov <eltimir.stoilkov.highschool@svetlina.net>
2023-01-12 15:28:49 +0200

    Initialize

---
+++ WebApplication2/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication2
+{
+    public class Car
+    {
+        public Guid? Id { get; set; }
+        public int Number { get; set; }
+        public string Name { get; set; }
+    }
+}
