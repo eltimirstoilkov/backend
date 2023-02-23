734b83b55b4962552d24ff661f8aa6726a7d8d71
eltimirstoilkov <eltimir.stoilkov.highschool@svetlina.net>
2023-02-23 10:48:42 +0200

    unused files

--- Persistence/Entities/v1/EngineGroup.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Persistence.Entities.v1;
-
-//ToDo - create a separate nom table
-public class EngineGroup
-{
-    public int Id { get; set; }
-
-    public int EngineCapacity { get; set; }
-}
