6269c0211962e327eac0ee1cf04f44c1290736c0
eltimirstoilkov <eltimir.stoilkov.highschool@svetlina.net>
2023-02-26 20:53:28 +0200

    Update TariffTypeRepository.cs

--- Persistence/Implementations/v1/TariffTypeRepository.cs
+++ Persistence/Implementations/v1/TariffTypeRepository.cs
@@ -15,42 +15,17 @@ public class TariffTypeRepository : ITariffTypeRepository
         _dbContext = dbContext;
     }
 
-    public VehicleTariffType Add(VehicleTariffType tariff)
-    {
-        EntityEntry<VehicleTariffType> newTariff = _dbContext.VehicleTariffTypes
-            .Add(tariff);
-        return newTariff.Entity;
-    }
-
-    public void Delete(VehicleTariffType tariff)
-    {
-        _dbContext.VehicleTariffTypes.Remove(tariff);
-    }
-
     public async Task<IList<VehicleTariffType>> GetAllAsync()
     {
         List<VehicleTariffType> all = await _dbContext.VehicleTariffTypes.ToListAsync();
         return all;
     }
 
-    public Task<VehicleTariffType?> GetByIdAsync(int id)
+    public async Task<VehicleTariffType?> GetByIdAsync(int id)
     {
-        var tariff = _dbContext.VehicleTariffTypes
+        VehicleTariffType? tariff = await _dbContext.VehicleTariffTypes
             .FirstOrDefaultAsync(x => x.Id == id);
+        
         return tariff;
     }
-
-    public Task<bool> IsExistingAsync(string description)
-    {
-        var result = _dbContext.VehicleTariffTypes
-            .AnyAsync(x => x.Description == description);
-        return result;
-    }
-
-    public async Task<int> SaveChangesAsync()
-    {
-        var result = await _dbContext.SaveChangesAsync();
-        return result;
-    }
-
 }
