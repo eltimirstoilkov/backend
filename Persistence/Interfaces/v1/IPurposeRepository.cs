a0e40a8b8a70429c8731370a54ac57d4bf143880
eltimirstoilkov <eltimir.stoilkov.highschool@svetlina.net>
2023-03-01 10:26:19 +0200

    Update IPurposeRepository.cs

--- Persistence/Interfaces/v1/IPurposeRepository.cs
+++ Persistence/Interfaces/v1/IPurposeRepository.cs
@@ -4,15 +4,7 @@ namespace Persistence.Interfaces.v1;
 
 public interface IPurposeRepository
 {
-    VehiclePurpose Add(VehiclePurpose vehiclePurpose);
-
-    void Delete(VehiclePurpose vehiclePurpose);
-
     Task<IList<VehiclePurpose>> GetAllAsync();
 
     Task<VehiclePurpose?> GetByIdAsync(int id);
-
-    Task<bool> IsExistingAsync(string description);
-
-    Task<int> SaveChangesAsync();
 }
